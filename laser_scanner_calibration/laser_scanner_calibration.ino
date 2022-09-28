c38d5484792d2039e2e05b470ef2ccf4e325c1f1
ayushchakra <achakraborty@olin.edu>
2022-09-28 01:38:56 -0400

    commented on calibration arduino sketch

--- laser_scanner_calibration/laser_scanner_calibration.ino
+++ laser_scanner_calibration/laser_scanner_calibration.ino
@@ -1,23 +1,38 @@
 #include <Servo.h>
+
+// Declare servo objects
 Servo servo_pan;
 Servo servo_tilt;
-int sensor_pin = A0;
-servo_pan_pin = 
+
+// Declare sensor pins for servo and infrared sensor
+int infrared_sensor_pin = A0;
+int servo_pan_pin = 9;
+int servo_tilt_pin = 11;
+
+// Define start angles for pan and tilt to make infrared
+// sensor flat and horizontal
+int pan_start_angle = 78;
+int tilt_start_angle = 41;
 
 void setup() {
-  // put your setup code here, to run once:
-  servo_pan.attach(9);
-  servo_tilt.attach(11);
-  pinMode(A0, INPUT);
+  // Initialize servo pins
+  servo_pan.attach(servo_pan_pin);
+  servo_tilt.attach(servo_tilt_pin);
 
+  // Initialize infrared pin for analog reading
+  pinMode(infrared_sensor_pin, INPUT);
+
+  // Initialize serial output to read sensor data
   Serial.begin(9600);
 
-  servo_pan.write(78);
-  servo_tilt.write(41);
+  // Write pan and tilt servos to flat and horizontal state
+  servo_pan.write(pan_start_angle);
+  servo_tilt.write(tilt_start_angle);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Constantly print the analog reading from the infrared sensor
+  // to be analzyed and used to calculate calibration curve
   Serial.println(analogRead(sensor_pin));
   delay(50);
 }
